cae2059c05cb91737698857e50aa6a3e6cf2ea2e
Akhdan Fadhilah <me@akhdan.dev>
2025-12-28 00:49:54 +0900

    [react] ch 9 create our first page

--- react-foundations/09_installing-nextjs/app/page.js
+++ react-foundations/09_installing-nextjs/app/page.js
@@ -1,10 +1,15 @@
+// nextjs uses file-system routing
+// this means that instead of defining routes in a separate file,
+// we create files and folders inside the "app" directory
+
 import { useState } from 'react';
 
 function Header({ title }) {
   return <h1>{title ? title : "Default title"}</h1>;
 }
 
-function HomePage() {
+// help nextjs distinguish which component to render as the main component of the page
+export default function HomePage() {
   const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
 
   const [likes, setLikes] = React.useState(0);

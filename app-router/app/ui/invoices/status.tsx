1e0715840513f22ea861ad34f24351315cb5c971
Akhdan Fadhilah <me@akhdan.dev>
2025-12-28 13:21:46 +0900

    [app] another css styling with cslx

--- app-router/app/ui/invoices/status.tsx
+++ app-router/app/ui/invoices/status.tsx
@@ -1,6 +1,7 @@
 import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 
+// clsx used for conditional class names
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
     <span

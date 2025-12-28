ea61c548ccf863eb7a6b1340b1fdb95e8c19ec3b
Akhdan Fadhilah <me@akhdan.dev>
2025-12-28 13:36:08 +0900

    [app] add font with next/font

--- app-router/app/layout.tsx
+++ app-router/app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts'
 
 export default function RootLayout({
   children,
@@ -7,7 +8,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* antialiased is tailwind css class for better font rendering */}
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
